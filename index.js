2c4871974f700cef04a6103971a583395f2cd2e8
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-09 17:16:48 +0300

    commit tree cfc1f02d8083120aafdf092ae0391b28c19ef18b

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Write your code here
+let num1 = 31, num2 = 2, num3 = 24, num4 = 5
+    const multiply = num1 * num2
+    const random = Math.floor((Math.random()*10))+1
+    const mod = num3 % num4
+    const max = Math.max(1,2,3,20)
